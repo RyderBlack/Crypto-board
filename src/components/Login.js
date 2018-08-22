037a99453dd7165a30d32556f0daf76c784233c9
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-22 15:44:09 +0200

    updated login style with bootstrap 4  v0.08

--- src/components/Login.js
+++ src/components/Login.js
@@ -64,7 +64,7 @@ export default class Login extends Component {
                   </div>
 
                   <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.login}>Log in</button>
-                  <button className="btn btn-lg btn-primary btn-block text-uppercase" onClick={this.signup}>Sign up</button>
+                  <button className="btn btn-lg btn-primary btn-block btn-signup text-uppercase" onClick={this.signup}>Sign up</button>
                   <hr className="my-4" />
                   <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2"></i> Sign in with Google</button>
                   <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
