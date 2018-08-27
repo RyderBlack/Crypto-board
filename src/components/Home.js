e801caa97b98fcc0cafaf17c20e6ceb5f83daa1d
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-27 19:08:07 +0200

    changing api to coinmarketcap

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,29 @@ export default class Home extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            currencies : []
+        }
         this.logout = this.logout.bind(this);
     }
 
     logout() {
         authFirebase.auth().signOut();
+    }
+
+    componentWillMount() {
+        fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&start=0&limit=10&cryptocurrency_type=tokens&convert=USD,BTC')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson.items);
+                this.setState({
+                    currencies: responseJson.items
+                });
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     
     render() {
