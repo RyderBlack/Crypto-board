6610b6b69f49d13826b52b0ce96df66abd28adb1
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-28 17:02:55 +0200

    node modif on api call

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&start=0&limit=10&cryptocurrency_type=tokens&convert=USD,BTC&key=aad925c0-a945-469f-bce2-632f03fa02f8';
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get(url, (req, res) => {
+  res.send({ express: res });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
