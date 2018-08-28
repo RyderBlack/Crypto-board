f2b445424ca5aa37706dc3e31600ebc4734fb258
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-28 18:17:22 +0200

    updating home comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,43 +26,11 @@ export default class Home extends Component {
           };
         axios.get('https://min-api.cryptocompare.com/data/all/coinlist',config)
           .then(res => {
-            console.log(res.data)
+            console.log(res.data.Data)
             const currencies = res.data;
             this.setState({ currencies: currencies });
           })
       }
-
-   /* componentWillMount() {
-        fetch('')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                console.log(responseJson.items);
-                this.setState({
-                    currencies: responseJson.items
-                });
-
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-    
-
-   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-    */
 
 
 
